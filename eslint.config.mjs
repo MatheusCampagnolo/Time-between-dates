84ace236cc5c9afcd68e885fc153e97a320584f5
Matheus Messias <130857698+MatheusCampagnolo@users.noreply.github.com>
2025-01-27 19:35:00 -0300

    chore: configure TypeScript, ESLint, Prettier, and Jest

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          ts: 'never',
+        },
+      ],
+    },
+  },
+
+  prettier,
+];
